a6cd915386d2f42a49ac666867b0be996384678d
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-10-28 17:02:53 +0300

    Create components. Api integration.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Coin from "./Coin";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=try&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => {
+        alert("Api error");
+      });
+  }, []);
+
+  const inputHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="coin__app">
+      <div className="coin__search">
+        <h1 className="coing__text">Search a currency</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin__input"
+            onChange={inputHandler}
+          />
+        </form>
+      </div>
+
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Coin.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) => {
+  return (
+    <div className="coin__container">
+      <div className="coin__row">
+        <div className="coin">
+          <img src={image} alt="coin" />
+          <h1>{name}</h1>
+          <p className="coin__symbol">{symbol}</p>
+        </div>
+        <div className="coin__data">
+          <p className="coin__price">₺{price}</p>
+          <p className="coin__volume">₺{volume.toLocaleString()}</p>
+          <p className={`coin__percent ${priceChange < 0 ? "red" : "green"}`}>
+            {priceChange.toFixed(2)}%
+          </p>
+          <p className="coin__marketcap">
+              Mkt Cap: ₺{marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
